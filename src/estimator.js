174ffe49d5cf90cbcacb9eb4447e104e55cec676
magdelineM <40407512+magdelineM@users.noreply.github.com>
2020-04-07 17:53:51 +0200

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
-const covid19ImpactEstimator = (data) => data;
-
+const covid19ImpactEstimator = (data) => {region: {
+name: "Africa",
+avgAge: 19.7,
+avgDailyIncomeInUSD: 5,
+avgDailyIncomePopulation: 0.71
+},
+periodType: "days",
+timeToElapse: 58,
+reportedCases: 674,
+population: 66622705,
+  totalHospitalBeds: 1380614},
+      
+  return {
+impact.currentlyInfected: {reportedCases*10},
+severeImpact.currentlyInfected: {reportedCases*50}
+};
+};
 export default covid19ImpactEstimator;
